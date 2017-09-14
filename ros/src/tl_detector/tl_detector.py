5b1efb4684219ce5ddf6532fbaefca3c0a175817
Marcus Erbar <marcus.erbar@gmail.com>
2017-09-14 12:55:41 -0700

    moved all class variables to the top
    if starting simulator before roslaunch, it errors out since the subscriber tries to access e.g. self.listener before it exists.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -30,6 +30,15 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+        
+        self.bridge = CvBridge()
+        self.light_classifier = TLClassifier()
+        self.listener = tf.TransformListener()
+
+        self.state = TrafficLight.UNKNOWN
+        self.last_state = TrafficLight.UNKNOWN
+        self.last_wp = -1
+        self.state_count = 0
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -50,16 +59,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
         self.dbg_img = rospy.Publisher('/dbg_img', Image, queue_size=1)
         # To view: rosrun image_view image_view image:=/dbg_img
-
-        self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
-        self.listener = tf.TransformListener()
-
-        self.state = TrafficLight.UNKNOWN
-        self.last_state = TrafficLight.UNKNOWN
-        self.last_wp = -1
-        self.state_count = 0
-
+        
         rospy.spin()
 
     def pose_cb(self, msg):
